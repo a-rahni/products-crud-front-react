2792c30474153cfd6363a08bcb2d7001afe69ab6
a rahni <rahniahmed@gmail.com>
2023-08-15 11:06:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Products from './components/Products';
+import Home from './components/Home';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // use state: pour gerer l'etat de bouton menu activé
+  const [currentRoute, setCurrentRoute] = useState()
+
+  // use event: pour gerer au demarrage l'etat de bouton menu activé
+  // traitement a la fin de rendu de composant
+  useEffect(()=>{
+    const path = window.location.pathname.toLocaleLowerCase();
+    console.log(path);
+    setCurrentRoute(path.slice(1,path.length))
+  }, [])
+
+  return (  
+    <BrowserRouter>
+      <nav className='m-1 p-1 border border-info'>
+        <ul className='nav na-pills'>
+          <li>
+            <Link 
+              onClick={()=>setCurrentRoute("home")}
+              className={currentRoute==="home"? "btn btn-info ms-1": "btn btn-outline-info ms-1"} 
+              to={"/home"}>Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={()=>setCurrentRoute("products")}
+              className={currentRoute==="products"? "btn btn-info ms-1": "btn btn-outline-info ms-1"} 
+              to={"/products"}>Products
+            </Link>
+          </li>
+
+        </ul>
+
+      </nav>
+
+      <Routes>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/products' element={<Products />}></Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
